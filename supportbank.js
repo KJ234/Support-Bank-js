64cb8a851102adc4d629586125c54d30546be16e
KJ234 <komaljaved109@gmail.com>
2022-09-28 16:30:19 +0100

    comments

--- supportbank.js
+++ supportbank.js
@@ -1,6 +1,12 @@
 
 import fs from "fs"
+
 import readlineSync from "readline-sync"
 
 let contents = fs.readFileSync('./Transactions2014.txt', "utf-8")
 console.log (contents)
+
+
+// class for account
+// class for transactions - to, form , narrative, amount, the date
+
