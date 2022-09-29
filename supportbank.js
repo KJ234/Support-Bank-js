8434f569424ff9dec42c7dd8715d6b2db203a387
Humayra Hayern <hayern786@hotmail.com>
2022-09-29 11:34:34 +0100

    humayras changes

--- supportbank.js
+++ supportbank.js
@@ -1,3 +1,14 @@
+const logger = log4js.getLogger('<filename>');
+
+log4js.configure({
+  appenders: {
+      file: { type: 'fileSync', filename: 'logs/debug.log' }
+  },
+  categories: {
+      default: { appenders: ['file'], level: 'debug'}
+  }
+});
+
 
 import fs from "fs"
 import  csv  from 'csv-parser'
@@ -5,15 +16,31 @@ import  csv  from 'csv-parser'
 import readlineSync from "readline-sync"
 
 let contents = fs.readFileSync('./Transactions2014.csv', "utf-8")
-// console.log (contents)
+console.log (contents)
 
 
 // class for account
 // class for transactions - to, form , narrative, amount, the date
 
 
-let people = ["Jon A","Stephen S","Laura B","Sam N","Todd","Georgia I","Sarah T","Tim L","Rob S","Ben B","Chris W","Dan W",],
-index = readlineSync.keyInSelect(people, 'Which account?');
-console.log('Ok, ' + people[index] + ' has ... pounds in his account.');
+let accounts = ["Ben B","Chris W","Dan W",],
+index = readlineSync.keyInSelect(accounts, 'Which animal?');
+console.log('Ok, ' + accounts[index] + ' has ... pounds in his account.');
 
 
+
+const results = [];
+
+fs.createReadStream('Transactions2014.csv')
+  .pipe(csv())
+  .on('data', (data) => results.push(data))
+  .on('end', () => {
+    console.log(results);
+    // [
+    //   { NAME: 'Daffy Duck', AGE: '24' },
+    //   { NAME: 'Bugs Bunny', AGE: '22' }
+    // ]
+  });
+
+
+  
