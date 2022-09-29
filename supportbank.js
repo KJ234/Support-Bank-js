b6fd5c4859333109eef48e5a2c6afadd2f59b52e
KJ234 <komaljaved109@gmail.com>
2022-09-29 10:53:11 +0100

    csv

--- supportbank.js
+++ supportbank.js
@@ -3,7 +3,7 @@ import fs from "fs"
 
 import readlineSync from "readline-sync"
 
-let contents = fs.readFileSync('./Transactions2014.txt', "utf-8")
+let contents = fs.readFileSync('./Transactions2014.csv', "utf-8")
 console.log (contents)
 
 
@@ -11,10 +11,7 @@ console.log (contents)
 // class for transactions - to, form , narrative, amount, the date
 
 
-
-
-
-
 let accounts = ["Ben B","Chris W","Dan W",],
 index = readlineSync.keyInSelect(accounts, 'Which animal?');
 console.log('Ok, ' + accounts[index] + ' has ... pounds in his account.');
+
