587bb92ab74e499d4f8353645c9099f000273e1f
KJ234 <komaljaved109@gmail.com>
2022-09-29 11:18:33 +0100

    adding names

--- supportbank.js
+++ supportbank.js
@@ -5,28 +5,15 @@ import  csv  from 'csv-parser'
 import readlineSync from "readline-sync"
 
 let contents = fs.readFileSync('./Transactions2014.csv', "utf-8")
-console.log (contents)
+// console.log (contents)
 
 
 // class for account
 // class for transactions - to, form , narrative, amount, the date
 
 
-let accounts = ["Ben B","Chris W","Dan W",],
-index = readlineSync.keyInSelect(accounts, 'Which animal?');
-console.log('Ok, ' + accounts[index] + ' has ... pounds in his account.');
+let people = ["Jon A","Stephen S","Laura B","Sam N","Todd","Georgia I","Sarah T","Tim L","Rob S","Ben B","Chris W","Dan W",],
+index = readlineSync.keyInSelect(people, 'Which account?');
+console.log('Ok, ' + people[index] + ' has ... pounds in his account.');
 
 
-
-const results = [];
-
-fs.createReadStream('Transactions2014.csv')
-  .pipe(csv())
-  .on('data', (data) => results.push(data))
-  .on('end', () => {
-    console.log(results);
-    // [
-    //   { NAME: 'Daffy Duck', AGE: '24' },
-    //   { NAME: 'Bugs Bunny', AGE: '22' }
-    // ]
-  });
