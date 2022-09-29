ed534aef7261d01e0d0aed377aadfea807be3f83
KJ234 <komaljaved109@gmail.com>
2022-09-29 11:36:03 +0100

    change

--- supportbank.js
+++ supportbank.js
@@ -36,10 +36,6 @@ fs.createReadStream('Transactions2014.csv')
   .on('data', (data) => results.push(data))
   .on('end', () => {
     console.log(results);
-    // [
-    //   { NAME: 'Daffy Duck', AGE: '24' },
-    //   { NAME: 'Bugs Bunny', AGE: '22' }
-    // ]
   });
 
 
