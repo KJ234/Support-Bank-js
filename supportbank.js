97227bf39c34bee3ed95c4addabbbb14582f4f2b
KJ234 <komaljaved109@gmail.com>
2022-09-29 09:47:09 +0100

    testing

--- supportbank.js
+++ supportbank.js
@@ -10,3 +10,11 @@ console.log (contents)
 // class for account
 // class for transactions - to, form , narrative, amount, the date
 
+
+
+
+
+
+let accounts = ["Ben B","Chris W","Dan W",],
+index = readlineSync.keyInSelect(accounts, 'Which animal?');
+console.log('Ok, ' + accounts[index] + ' has ... pounds in his account.');
